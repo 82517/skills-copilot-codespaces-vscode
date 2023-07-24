46e9588431bd95b79939e7bada7675f68e9c517f
82517 <138934064+82517@users.noreply.github.com>
2023-07-24 08:25:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,10 @@
-// create web server with express
+//Create web server
+//create web server
 const express = require('express');
 const app = express();
-const port = 3000;
-// import cors to allow cross-origin resource sharing
-const cors = require('cors');
-// import body-parser to parse JSON data
-const bodyParser = require('body-parser');
-// import mongoose to connect to MongoDB
-const mongoose = require('mongoose');
-// import routes
-const commentsRoutes = express.Router();
-// import model
-let Comment = require('./models/comments.model');
-
-// connect to MongoDB
-mongoose.connect('mongodb://
-
- 
+app.use(express.json());
+const port = 3000
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+//const { v4: uuidv4 } = require('uui
